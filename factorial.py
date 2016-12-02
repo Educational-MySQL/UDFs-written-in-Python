b135604379fcf60cb38a53fbcb45ab926b44548d
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2016-12-02 12:36:07 +0400

    Adde preliminary fix switched from recursion for factorial for issue #8

--- factorial.py
+++ factorial.py
@@ -3,13 +3,20 @@
 # Python program to find the factorial of a number using recursion
 import sys
 
-def recur_factorial(n):
-   """Function to return the factorial
-   of a number using recursion"""
-   if n == 1:
-       return n
-   else:
-       return n*recur_factorial(n-1)
+# def recur_factorial(n):
+#    """Function to return the factorial
+#    of a number using recursion"""
+#    if n == 1:
+#        return n
+#    else:
+#        return n*recur_factorial(n-1)
+
+# def factorial(n):
+#     num = 1
+#     while n >= 1:
+#         num = num * n
+#         n = n - 1
+#     return num
 
 def main():
     #print(len(sys.argv))
@@ -22,8 +29,14 @@ def main():
         elif num == 0:
             sys.stdout.write(str(1))
         else:
-            retnum = recur_factorial(num)
-            sys.stdout.write(str(retnum))
+            result_num = 1
+            while num >= 1:
+                result_num = result_num * num
+                num = num - 1
+            # for i in range(num):
+            #     result_num *= i + 1 
+            #retnum = recur_factorial(num)
+            sys.stdout.write(str(result_num))
             
             
     else:
